5e969fecbe5e56cb9b2f3e962018e19eea91257b
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-09-30 21:48:13 +0200

    feat: add sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ MatToolbarModule, MatSidenavModule, MatIconModule ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
